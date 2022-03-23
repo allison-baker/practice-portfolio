aa5954d17c3f9bbe6bff1e0a277bcdc868701740
allison-baker <baker.allison.m@gmail.com>
2022-03-22 22:17:08 -0600

    start starships page

---
+++ starships/sw-starships.js
@@ -0,0 +1,14 @@
+import { starships } from "../data/starships.js";
+
+let main = document.querySelector("#main");
+let nav = document.querySelector("#nav");
+let navList = document.querySelector("#navList");
+let shipViewer = document.querySelector("#shipViewer");
+
+function populateNav() {
+
+}
+
+function populateShipView() {
+    
+}
