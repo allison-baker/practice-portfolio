66ccb5f96c7d3ab62fd7ae937af3089b6a42409f
allison-baker <baker.allison.m@gmail.com>
2022-04-05 10:47:10 -0600

    added congress functionality

--- congress/congress.js
+++ congress/congress.js
@@ -1,61 +1,116 @@
 import { senators } from "../data/senators.js";
+import { representatives } from "../data/representatives.js";
 import { removeChildren } from "../utilities/index.js";
 
-const senatorGrid = document.querySelector("#senatorGrid");
-const mostSenior = document.querySelector("#mostSenior");
-const loyaltyList = document.querySelector("#mostLoyal");
+const nav = document.querySelector("#nav");
+const info = document.querySelector("#info");
+const congressGrid = document.querySelector("#congressGrid");
 
-function simplifySenators() {
-    return senators.map(senator => {
-        const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `;
-        return {
-            id: senator.id,
-            name: `${senator.first_name}${middleName}${senator.last_name}`,
-            party: senator.party,
-            gender: senator.gender,
-            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
-            seniority: +senator.seniority,
-            missedVotesPct: senator.missed_votes_pct,
-            loyaltyPct: senator.votes_with_party_pct,
-            nextElection: senator.next_election,
-            state: senator.state
-        }
-    })
+// Simplify members of either array
+function simplifyMembers(arr) {
+  return arr.map((member) => {
+    const middleName = member.middle_name ? ` ${member.middle_name} ` : ` `;
+    return {
+      id: member.id,
+      name: `${member.first_name}${middleName}${member.last_name}`,
+      party: member.party,
+      gender: member.gender,
+      imgURL: `https://www.govtrack.us/static/legislator-photos/${member.govtrack_id}-200px.jpeg`,
+      seniority: +member.seniority,
+      missedVotesPct: member.missed_votes_pct,
+      loyaltyPct: member.votes_with_party_pct,
+      nextElection: member.next_election,
+      state: member.state,
+    };
+  });
 }
 
-const simpleSenators = simplifySenators();
+// Use function to create simplified arrays
+const simpleSenators = simplifyMembers(senators);
+const simpleReps = simplifyMembers(representatives);
 
-function populateGrid(senatorArr) {
-    removeChildren(senatorGrid);
-    
-    senatorArr.forEach(senator => {
-        const senatorFig = document.createElement('figure');
-        const senatorImg = document.createElement('img');
-        const senatorCap = document.createElement('figcaption');
+// Create buttons to switch between senate and reps
+const senatorsButton = document.createElement("button");
+senatorsButton.textContent = "Show Senators";
+senatorsButton.addEventListener("click", () => {
+  populateGrid(simpleSenators, "the Senate");
+  populateInfo(simpleSenators, "the Senate")
+});
+nav.appendChild(senatorsButton);
 
-        senatorImg.src = senator.imgURL;
-        senatorImg.alt = `photo of ${senator.name}`;
+const repsButton = document.createElement("button");
+repsButton.textContent = "Show House of Representatives";
+repsButton.addEventListener("click", () => {
+  populateGrid(simpleReps, "the House of Representatives");
+  populateInfo(simpleReps, "the House of Representatives");
+});
+nav.appendChild(repsButton);
 
-        senatorCap.textContent = `${senator.name}`;
+// Function to populate grid with either array
+function populateGrid(congressArr, string) {
+  removeChildren(congressGrid);
+  removeChildren(info);
 
-        senatorFig.appendChild(senatorImg);
-        senatorFig.appendChild(senatorCap);
-        senatorGrid.appendChild(senatorFig);
-    })
+  const title = document.createElement("h2");
+  title.textContent = `Members of ${string}`;
+  info.appendChild(title);
+
+  congressArr.forEach((member) => {
+    const memberFig = document.createElement("figure");
+    const memberImg = document.createElement("img");
+    const memberCap = document.createElement("figcaption");
+
+    memberImg.src = member.imgURL;
+    memberImg.alt = `photo of ${member.name}`;
+
+    memberCap.textContent = `${member.name}`;
+
+    memberFig.appendChild(memberImg);
+    memberFig.appendChild(memberCap);
+    congressGrid.appendChild(memberFig);
+  });
 }
 
-const mostSeniorMember = simpleSenators.reduce((acc, senator) => {
-    return acc.seniority > senator.seniority ? acc : senator;
-})
+// Populate info div with facts
+function populateInfo(arr, string) {
+  removeChildren(info);
 
-mostSenior.textContent = `The most senior member of the Senate is ${mostSeniorMember.name}.`;
+  let mostSenior = document.createElement("p");
+  let loyaltyParagraph = document.createElement("p");
+  let loyaltyList = document.createElement("ul");
 
-simpleSenators.forEach(senator => {
-    if (senator.loyaltyPct === 100) {
-        let listItem = document.createElement("li");
-        listItem.textContent = senator.name;
-        loyaltyList.appendChild(listItem);
+  const mostSeniorMember = arr.reduce((acc, member) => {
+    return acc.seniority > member.seniority ? acc : member;
+  });
+
+  const biggestMissedVotesPct = arr.reduce((acc, member) => {
+    return acc.missedVotesPct > member.missedVotesPct ? acc : member;
+  });
+
+  const biggestVacationerList = arr
+    .filter((member) => member.missedVotesPct === biggestMissedVotesPct.missedVotesPct)
+    .map((member) => member.name)
+    .join(" and ");
+
+  mostSenior.textContent = `The most senior member of ${string} is ${mostSeniorMember.name} and the biggest vacationers are ${biggestVacationerList}.`;
+
+  loyaltyParagraph.textContent = `The members from ${string} who have 100% voting loyalty to their parties are:`
+
+  arr.forEach((member) => {
+    if (member.loyaltyPct === 100) {
+      let listItem = document.createElement("li");
+      listItem.textContent = member.name;
+      loyaltyList.appendChild(listItem);
     }
-})
+  });
 
-populateGrid(simpleSenators);
+  info.appendChild(mostSenior);
+  info.appendChild(loyaltyParagraph);
+  info.appendChild(loyaltyList);
+}
+
+// TODO considerations for final project:
+//   -sort by party
+//   -sort by party and gender with a count
+//   -better styling of the grid of senators and their names
+//   -include more data in grid
