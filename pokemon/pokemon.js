7f62aa5fb92689a3b03656b1a2c91a63c6465611
allison-baker <baker.allison.m@gmail.com>
2022-04-07 11:15:43 -0600

    create cards

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -1,16 +1,54 @@
+const pokeGrid = document.querySelector('#pokeGrid');
+
 let baseURL = "https://pokeapi.co/api/v2/pokemon";
 
-function getAPIData(url) {
+const getAPIData = async (url) => {
     try {
-        return fetch(url).then((data) => data.json())
+        const result = await fetch(url);
+        return await result.json();
     } catch (error) {
         console.error(error);
     }
 }
 
-function loadPokemon() {
-    let url = baseURL + "/ditto";
-    getAPIData(url).then((data) => console.log(data));
+async function loadPokemon() {
+    let url = baseURL + "?limit=100&offset=0";
+    console.log(url);
+    const pokeData = await getAPIData(url);
+    populateGrid(pokeData.results);
+}
+
+function populateGrid(pokemonArr) {
+    pokemonArr.forEach((pokemon) => {
+        createCard(pokemon);
+    });
+}
+
+function createCard(pokemon) {
+    const pokeScene = document.createElement('div');
+    pokeScene.className = 'scene';
+    const pokeCard = document.createElement('div');
+    pokeCard.className = 'card';
+    pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'));
+
+    pokeCard.appendChild(populateCardFront(pokemon));
+    pokeScene.appendChild(pokeCard);
+    pokeGrid.appendChild(pokeScene);
+}
+
+function populateCardFront(pokemon) {
+    const pokeFront = document.createElement('figure');
+    pokeFront.className = 'cardFace';
+
+    const pokeImg = document.createElement('img');
+    pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png`;
+    
+    const pokeCap = document.createElement('figcaption');
+    pokeCap.textContent = 'Snorlax';
+
+    pokeFront.appendChild(pokeImg);
+    pokeFront.appendChild(pokeCap);
+    return pokeFront;
 }
 
 loadPokemon();
