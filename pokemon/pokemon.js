ad37bd35d2994372691a0d4c371c095dc2152ca0
allison-baker <baker.allison.m@gmail.com>
2022-04-19 11:24:00 -0600

    fix form clearing

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -12,6 +12,59 @@ const getAPIData = async (url) => {
   }
 };
 
+// Create Pokemon cards
+function populateCardFront(pokemon) {
+  const pokeFront = document.createElement("figure");
+  pokeFront.className = "cardFace front";
+
+  const pokeImg = document.createElement("img");
+  if (pokemon.id === 0) {
+    pokeImg.src = '../images/pokeball.png';
+  } else {
+    pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+  }
+  
+  const pokeCap = document.createElement("figcaption");
+  pokeCap.textContent = pokemon.name.toUpperCase();
+  //pokeCap.style.setProperty('background-color', getColor)
+
+  pokeFront.appendChild(pokeImg);
+  pokeFront.appendChild(pokeCap);
+  return pokeFront;
+}
+
+function populateCardBack(pokemon) {
+  const pokeBack = document.createElement("div");
+  pokeBack.className = "cardFace back";
+
+  const label = document.createElement("h4");
+  label.textContent = "Abilities";
+  pokeBack.appendChild(label);
+
+  const abilityList = document.createElement("ul");
+  pokemon.abilities.forEach((abilityItem) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = abilityItem.ability.name;
+    abilityList.appendChild(listItem);
+  });
+  pokeBack.appendChild(abilityList);
+
+  return pokeBack;
+}
+
+function populatePokeCard(pokemon) {
+  const pokeScene = document.createElement("div");
+  pokeScene.className = "scene";
+  const pokeCard = document.createElement("div");
+  pokeCard.className = "card";
+  pokeCard.addEventListener("click", () => pokeCard.classList.toggle("is-flipped"));
+
+  pokeCard.appendChild(populateCardFront(pokemon));
+  pokeCard.appendChild(populateCardBack(pokemon));
+  pokeScene.appendChild(pokeCard);
+  pokeGrid.appendChild(pokeScene);
+}
+
 // Create new Pokemon
 class Pokemon {
   constructor(name, height, weight, abilities, types) {
@@ -59,68 +112,28 @@ form.addEventListener("submit", (event) => {
 
   console.log(newPokemon);
   populatePokeCard(newPokemon);
-});
 
-// Create Pokemon cards
-function populatePokeCard(pokemon) {
-  const pokeScene = document.createElement("div");
-  pokeScene.className = "scene";
-  const pokeCard = document.createElement("div");
-  pokeCard.className = "card";
-  pokeCard.addEventListener("click", () => pokeCard.classList.toggle("is-flipped"));
-
-  pokeCard.appendChild(populateCardFront(pokemon));
-  pokeCard.appendChild(populateCardBack(pokemon));
-  pokeScene.appendChild(pokeCard);
-  pokeGrid.appendChild(pokeScene);
-}
-
-function populateCardFront(pokemon) {
-  const pokeFront = document.createElement("figure");
-  pokeFront.className = "cardFace front";
-
-  const pokeImg = document.createElement("img");
-  if (pokemon.id === 0) {
-    pokeImg.src = '../images/pokeball.png';
-  } else {
-    pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+  for (let i=0; i < form.elements.length-1; i++) {
+    form.elements[i].value = '';
   }
-  
-
-  const pokeCap = document.createElement("figcaption");
-  pokeCap.textContent = pokemon.name.toUpperCase();
-
-  pokeFront.appendChild(pokeImg);
-  pokeFront.appendChild(pokeCap);
-  return pokeFront;
-}
-
-function populateCardBack(pokemon) {
-  const pokeBack = document.createElement("div");
-  pokeBack.className = "cardFace back";
-
-  const label = document.createElement("h4");
-  label.textContent = "Abilities";
-  pokeBack.appendChild(label);
-
-  const abilityList = document.createElement("ul");
-  pokemon.abilities.forEach((abilityItem) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = abilityItem.ability.name;
-    abilityList.appendChild(listItem);
-  });
-  pokeBack.appendChild(abilityList);
-
-  return pokeBack;
-}
+});
 
 // Load page
 async function loadPokemon(offset = 0, limit = 25) {
   const data = await getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
   for (const pokeData of data.results) {
     const pokemon = await getAPIData(pokeData.url);
-    populatePokeCard(pokemon);
+    const simplifiedPokemon = {
+      id: pokemon.id,
+      height: pokemon.height,
+      weight: pokemon.weight,
+      name: pokemon.name,
+      types: pokemon.types,
+      abilities: pokemon.abilities,
+      moves: pokemon.moves.slice(0, 3)
+    }
+    populatePokeCard(simplifiedPokemon);
   }
 }
 
-loadPokemon();
+await loadPokemon();
